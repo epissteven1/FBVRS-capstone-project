2ca16b2b538373b71d7bfc809b8c469b2c4f4bcc
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-09-01 05:27:13 -0400

    Update AppDescription.py

--- App/App_Pages/AppDescription.py
+++ App/App_Pages/AppDescription.py
@@ -102,7 +102,7 @@ def app():
             </div>
         </body>
         """, unsafe_allow_html=True)
-    st.image('App//App_Images//FC.PNG', use_column_width=True)
+    st.image(image="https://github.com/epissteven1/FBVRS-capstone-project/blob/master/App/App_Images/FC.png?raw=true", use_column_width=True)
     st.markdown("""
         <style>
             .topnav {
