2346c2e7d0393d82aa7bc76a2376b6cfed43a2ac
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-09-01 08:27:46 -0400

    Update testing.py

--- App/App_Pages/testing.py
+++ App/App_Pages/testing.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def app:
+def app():
 st.write("hello")
 print("Current working directory:", os.getcwd())
 print("Image directory path:", os.path.join(os.path.dirname(__file__), image_dir))
