b93736924b93538734efaf4f9516bde149a9a548
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-09-01 06:37:47 -0400

    Update Record.py

--- App/App_Pages/Record.py
+++ App/App_Pages/Record.py
@@ -122,17 +122,5 @@ def app():
         if os.path.exists(temp_audio_file):
             os.remove(temp_audio_file)
             
-
-
-# Go up one directory from the current script location to access the 'App' directory
-app_dir = os.path.dirname(os.path.dirname(__file__))
-
-# Construct the path to the 'Image' directory inside 'App'
-image_path = os.path.join(app_dir, "Image", "Ga.png")
-
-st.write("Constructed Image Path:", image_path)
-st.image(image_path)
-
-
 if __name__ == "__main__":
     app()
