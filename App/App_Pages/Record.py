1857606f6cb100abf99767885562cff747793bc7
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-09-01 06:03:40 -0400

    Update Record.py

--- App/App_Pages/Record.py
+++ App/App_Pages/Record.py
@@ -123,18 +123,9 @@ def app():
             os.remove(temp_audio_file)
             
 
+image_dir = os.path.join(os.path.dirname(__file__), "../Image")
+st.write("Files in 'Image' Directory:", os.listdir(image_dir))
 
-
-
-
-# Go up one directory from the current script location to access the 'App' directory
-script_dir = os.path.dirname(__file__)
-app_dir = os.path.dirname(script_dir)
-
-# Construct the path to the 'Image' directory inside 'App'
-image_path = os.path.join(app_dir, "Image", "ga.png")
-
-st.image(image_path)
 
 
 
