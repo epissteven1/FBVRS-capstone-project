fbf40197985f997d169f355e18a7af65c049e8e0
Steven Epis <114670616+epissteven1@users.noreply.github.com>
2024-09-02 23:04:11 -0400

    Update Baybayin_App.py

--- App/Baybayin_App.py
+++ App/Baybayin_App.py
@@ -21,7 +21,7 @@ def get_img_as_base64(file):
     return base64.b64encode(data).decode()
 
 
-img = get_img_as_base64("App/App_Images/Sidebar.png")
+img = get_img_as_base64("App/App_Images/Sidebar1.png")
 
 # Custom CSS for page styling
 st.markdown(f"""
